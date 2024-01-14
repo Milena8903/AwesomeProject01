001fd01a3558932b110ac6642b093ad657054c0c
Milena8903 <milena_8903@hotmail.com>
2024-01-14 12:31:09 -0500

    first commit

---
+++ AppPro.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme,
+} from 'react-native'
+
+
+//Envuelvo el texto en la vista, hoja de estilos
+function AppPro():JSX.Element{
+    const isDarkMode = useColorScheme() === 'dark'
+    return(
+        <View style={styles.container}>
+            <Text style={isDarkMode ? styles.whiteText:
+            styles.whiteText}>
+                Hello World 
+            </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    whiteText:{
+        color: '#FFFFFF'
+    },
+    darkText:{
+        color: '#000000'
+    }
+})
+
+export default AppPro
